e579e04423aab2b56cdd9d07379e5cabd8e68508
laxsamy <laxsamy@gmail.com>
2023-02-05 19:57:04 +0000

    Jira ticket committing project for first time

---
+++ src/test/java/uk/axone/devintest/git/tests/AppTests.java
@@ -0,0 +1,11 @@
+package uk.axone.devintest.git.tests;
+
+import jdk.jfr.StackTrace;
+import org.junit.Test;
+
+public class AppTests {
+    @Test
+    public void test1(){
+        System.out.println("Running test1");
+    }
+}
